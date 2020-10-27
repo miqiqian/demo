d4b1338a2eb2850e55f35b2501f18ce128df0aa5
sdujqy2011@163.com <sdujqy2011@163.com>
2020-10-27 15:43:14 +0800

    test

--- src/test/java/com/example/demo/controller/ApiControllerTest.java
+++ src/test/java/com/example/demo/controller/ApiControllerTest.java
@@ -1,13 +1,41 @@
 package com.example.demo.controller;
 
+import com.example.demo.DemoApplicationTests;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ApiControllerTest {
+class ApiControllerTest extends DemoApplicationTests {
 
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mvc;
+
+    @BeforeEach
+    public  void setupMockMvc(){
+        mvc = MockMvcBuilders.webAppContextSetup(wac).build(); //初始化MockMvc对象
+    }
+    
     @Test
-    void getAllUser() {
+    void getAllUser() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/user/list")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
     }
 
     @Test
